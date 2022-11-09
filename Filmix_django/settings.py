9ea3d37bea817ba222dd08a8e9e559944d9bc0cf
Ramiro Grande <rd.grande@hotmail.com>
2022-11-09 18:16:37 -0300

    Requirements y setting.py por la base de datos

--- Filmix_django/settings.py
+++ Filmix_django/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'Filmix_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'filmix_django',
+        'USER': 'postgres',
+        'PASSWORD': 'filmix_1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
