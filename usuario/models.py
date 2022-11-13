808641f0d4f829f5b00926d43daf8c7c5c73668c
Ramiro Grande <rd.grande@hotmail.com>
2022-11-13 12:58:52 -0300

    Agregado py_cache de aplicacion usuario a gitignore y avances en models.py

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class usuario(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    apellido = models.CharField(max_length=100, verbose_name='Apellido')
+    mail = models.EmailField(verbose_name='Email')
+    contraseña = models.CharField(max_length=100, verbose_name='Contraseña')
+
+
+class pelicula(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    # imagen = models.ImageField(verbose_name='Imagen')
+    fecha_ini = models.DateField(verbose_name='Fecha de Inicio')
+    fecha_fin = models.DateField(verbose_name='Fecha de Fin')
+    horarios = 
+    duracion = models.DurationField(verbose_name='Duracion')
+    descripcion = models.TextField(verbose_name='Descripcion')
+    trailer = models.SlugField(verbose_name='Trailer')
+    
+
+
+""" var pelicula_0 = {
+    id: 1,
+    nombre: "Granizo",
+    imagen: "./img/Peliculas/Granizo.jpg",
+    fecha_ini: "01/03/2022",
+    fecha_fin: "31/12/2022",
+    horarios: ["20:00","22:00"],
+    descripcion: "Un famoso meteorólogo de la televisión se convierte en el enemigo público número uno cuando no logra evitar una terrible tormenta de granizo.",
+    trailer: "https://www.youtube.com/embed/-F2watcvQQs",
+} """
