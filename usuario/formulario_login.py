1222c5b6dc8ce9bffb57832a46a4baae9f3d3773
Ramiro Grande <rd.grande@hotmail.com>
2022-11-28 22:01:07 -0300

    Autenticación (revisión 0)

--- usuario/formulario_login.py
+++ usuario/formulario_login.py
@@ -1,8 +1,6 @@
 from django import forms
-from django.forms import ValidationError
 
-
-class formulario_registro(forms.Form):
+class formulario_login(forms.Form):
     nombre = forms.CharField(label=False, required=True, max_length=100, 
                             error_messages={'required':'Por favor complete su nombre'},
                             widget=forms.TextInput(attrs={'class':'cont-input', 'placeholder':'Ingrese su nombre'} ))
@@ -18,15 +16,3 @@ class formulario_registro(forms.Form):
     contraseña_2  = forms.CharField(label=False, required=True, max_length=100,
                             error_messages={'required':'Por favor ingrese su contraseña'},
                             widget=forms.PasswordInput(attrs={'class':'cont-input', 'placeholder':'Ingrese nuevamente su contraseña'} ))
-
-    # Controla que las contraseñas introducidas sean iguales
-
-    def clean(self):
-        cleaned_data = super(formulario_registro, self).clean()
-        password = cleaned_data.get("contraseña_1")
-        confirm_password = cleaned_data.get("contraseña_2")
-
-        if password != confirm_password:
-            raise forms.ValidationError(
-                "Las contraseñas ingresadas no coinciden"
-            )
--- usuario/urls.py
+++ usuario/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path('detalle_pelicula/<str:pelicula>/', views.detalle_pelicula, name='detalle_pelicula'),
     path('registrarse/', views.registrarse, name='registrarse'),
     path('inicio_sesion/', views.inicio_sesion, name='inicio_sesion'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='index.html'), name='logout'),
 ]
