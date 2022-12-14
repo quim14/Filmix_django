6e62bb46e4089f70fccf87c42ee4108d5e639d04
Ramiro Grande <rd.grande@hotmail.com>
2022-12-13 23:24:59 -0300

    Actualizacion 2 - 13/12/2022

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from administracion.models import Pelicula, Funcion, Horario
+
+class PeliculaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'baja')
+    search_fields = ['nombre']
+    list_filter = ('baja',)
+
+class FuncionAdmin(admin.ModelAdmin):
+    pass
+
+class HorarioAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Pelicula, PeliculaAdmin)
+admin.site.register(Funcion, FuncionAdmin)
+admin.site.register(Horario, HorarioAdmin)
