099c2a711bd59b8e038883fa753b546c0082a83d
Ramiro Grande <rd.grande@hotmail.com>
2022-12-15 17:56:52 -0300

    urls.py ususario

--- usuario/urls.py
+++ usuario/urls.py
@@ -6,8 +6,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cartelera/', views.cartelera, name='cartelera'),
-    path('detalle_pelicula/<str:pelicula>/', views.detalle_pelicula, name='detalle_pelicula'),
+    path('detalle_pelicula/<int:id_pelicula>', views.detalle_pelicula, name='detalle_pelicula'),
     path('registrarse/', views.registrarse, name='registrarse'),
     path('inicio_sesion/', views.inicio_sesion, name='inicio_sesion'),
     path('logout/', auth_views.LogoutView.as_view(template_name='usuario/index.html'), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
