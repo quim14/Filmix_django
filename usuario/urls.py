dc8a93d3558dfd8f669c0b70572d7e2cbe680203
Ramiro Grande <rd.grande@hotmail.com>
2022-12-15 18:01:04 -0300

    urls.py usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
